09c8031310fd1d437c8d8be38fd2aa7487d526ed
Gekctek <Gekctek@Gmail.com>
2015-08-09 16:25:39 -0700

    Removing random controller. using RpcSection classes instead

---
+++ src/JsonRpc.Router.Sample/RpcSections/CommandRpcSection.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JsonRpc.Router.Sample.RpcSections
+{
+	public class CommandRpcSection
+	{
+		public void DoStuff()
+		{
+
+		}
+
+		private void HiddenMethod()
+		{
+
+		}
+	}
+}
---
+++ src/JsonRpc.Router.Sample/RpcSections/StringRpcSection.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JsonRpc.Router.Sample.RpcSections
+{
+	public class StringRpcSection
+	{
+		public int CharacterCount(string text)
+		{
+			if (text == null)
+			{
+				return -1;
+			}
+			return text.Count();
+		}
+	}
+}
