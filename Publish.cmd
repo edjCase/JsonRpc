cce5c8533b47fa01e34092969be83a8285807cf3
Gekctek <Gekctek@Gmail.com>
2015-10-27 13:36:21 -0700

    Adding publish script

--- Publish.cmd
+++ Publish.cmd
@@ -1,6 +1,8 @@
 SET configuration=Release
 SET out=C:/Publish
 
-cd src/JsonRpc.Core
+call dnu pack ./src/JsonRpc.Core --configuration %configuration% --out "%out%/JsonRpc.Core"
 
-dnu pack --configuration %configuration% --out "%out%/JsonRpc.Core"
+call dnu pack ./src/JsonRpc.Router --configuration %configuration% --out "%out%/JsonRpc.Router"
+
+call dnu pack ./src/JsonRpc.Client --configuration %configuration% --out "%out%/JsonRpc.Client"
