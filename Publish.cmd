2987ee55dd233fc05eb894a2cd3ec82cf1c808aa
Ethan Celletti <Gekctek@Gmail.com>
2020-01-20 00:40:48 -0800

    Minor

--- Publish.cmd
+++ Publish.cmd
@@ -1,8 +1,10 @@
 SET configuration=Release
 SET out=C:/Publish
 
-call dotnet test ./test/EdjCase.JsonRpc.Router.Tests -c Release
+call dotnet test ./test/EdjCase.JsonRpc.Router.Tests -c %configuration%
 
 call dotnet pack ./src/EdjCase.JsonRpc.Router -c %configuration% -o "%out%/EdjCase.JsonRpc.Router"
+
+call dotnet test ./test/EdjCase.JsonRpc.Client.Tests -c %configuration%
 
 call dotnet pack ./src/EdjCase.JsonRpc.Client -c %configuration% -o "%out%/EdjCase.JsonRpc.Client"
