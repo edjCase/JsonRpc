7b3fdda885087e1ad8113d94324f9ffdb42d662e
Gekctek <gekctek@gmail.com>
2022-07-09 10:41:54 -0700

    Updating to .net 6

--- publish.sh
+++ publish.sh
@@ -4,6 +4,6 @@ dotnet test ./test/EdjCase.JsonRpc.Router.Tests -c Release
 
 dotnet pack ./src/EdjCase.JsonRpc.Router -c Release  -o "./out/EdjCase.JsonRpc.Router"
 
-dotnet pack ./src/EdjCase.JsonRpc.Router -c Release  -o "./out/EdjCase.JsonRpc.Router.Swagger"
+dotnet pack ./src/EdjCase.JsonRpc.Router.Swagger -c Release  -o "./out/EdjCase.JsonRpc.Router.Swagger"
 
 dotnet pack ./src/EdjCase.JsonRpc.Client -c Release -o "out/EdjCase.JsonRpc.Client"
